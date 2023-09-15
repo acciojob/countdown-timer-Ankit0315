bab7313992364e9dbcd2ced0a960a5cef83eece9
ANKIT KUMAR RAI <117643219+Ankit0315@users.noreply.github.com>
2023-09-15 10:09:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,29 @@
-const durationInput = document.getElementById('userInput');
-const startButton = document.querySelector('.timer button');
-const countdownDisplay = document.getElementById('countDown');
-const endTimeDisplay = document.getElementById('endTime');
-let countdown;
+// Your script here.
+const countDownDisplay=document.querySelector("#countDown");
+const endTimeDisplay=document.querySelector("#endTime");
 
-// Add click event listener to start button
-startButton.addEventListener('click', function () {
-  startTimer(durationInput.value * 60);
-});
+document.querySelector("button").addEventListener("click",()=>{
+    let countDownTime=document.querySelector("input").value;
+    let currDate=new Date();
+    let endTime=addMinutes(currDate,Number(countDownTime));
+    console.log(endTime);
+    countDownDisplay.textContent=currDate.toLocaleTimeString();
+    endTimeDisplay.textContent=formatEndTime(endTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }));
+    endTime=endTime.toLocaleTimeString();
+    let displayTime = setInterval(()=>{
+        let currTime = new Date().toLocaleTimeString();
+        countDownDisplay.textContent=currTime;
+        if( currTime==endTime) clearInterval(displayTime);
+    },1000)
+})
 
-function startTimer(duration) {
-  const endTime = Date.now() + duration * 1000;
-  clearInterval(countdown);
-  countdown = setInterval(function () {
-    const secondsLeft = Math.round((endTime - Date.now()) / 1000);
-    if (secondsLeft < 0) {
-      clearInterval(countdown);
-      countdownDisplay.textContent = "Countdown has ended!";
-      endTimeDisplay.textContent = "";
-      return;
-    }
-    const minutes = Math.floor(secondsLeft / 60);
-    const seconds = secondsLeft % 60;
-    const endDateTime = new Date(endTime);
-    const endHour = endDateTime.getHours();
-    const endMinute = endDateTime.getMinutes();
-    countdownDisplay.textContent = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
-    endTimeDisplay.textContent = `Ends at ${endHour}:${endMinute < 10 ? '0' : ''}${endMinute}`;
-  }, 1000);
+const addMinutes=(date,mins)=>{
+    date.setMinutes(date.getMinutes()+mins);
+    return date;
+}
+
+const formatEndTime=(timeString)=>{
+    const [hours, minutes, meridiem] = timeString.split(/:| /);
+    const formattedTimeString = `${parseInt(hours, 10)}:${minutes} ${meridiem}`;
+    return formattedTimeString;
 }
